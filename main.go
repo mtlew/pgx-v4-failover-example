4513c2a007432617c91017f6b323bead33d115a0
Mikhail <m.mochalov@greatbook.ru>
2023-08-01 17:46:28 +0300

    Concurrency

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "log"
 // https://github.com/jackc/pgx/pull/545
 // https://www.postgresql.org/docs/current/libpq-connect.html + Ctrl+F = target_session_attrs
 const connString = "postgres://postgres:example@localhost:5436,localhost:5437/postgres?pool_max_conns=1000" // ?target_session_attrs=any
-const goroutines = 1
+const goroutines = 5
 
 func main() {
 	s := newStorage(newConn(connString))
